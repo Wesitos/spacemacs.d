8b859176485f5a44c57bbe55d26ca47cd15ae909
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2016-08-19 11:00:11 -0500

    Js options

--- init.el
+++ init.el
@@ -370,6 +370,9 @@ you should place you code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(js2-indent-switch-body t)
+ '(js2-mode-assume-strict t)
+ '(js2-mode-show-strict-warnings nil)
  '(magit-commit-arguments (quote ("--gpg-sign=75D9D625ABF40969"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
