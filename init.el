5794f19a229ca41177a9af207737c5a3571aa803
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2016-11-22 14:45:42 -0500

    Enable snippets in auto-completion

--- init.el
+++ init.el
@@ -20,6 +20,7 @@ values."
    '(
      ruby
      (auto-completion :variables
+                      auto-completion-enable-snippets-in-popup t
                       auto-completion-enable-help-tooltip t
                       auto-completion-enable-sort-by-usage t)
      syntax-checking
