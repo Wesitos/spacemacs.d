0271463baf84063eafa469a951e75d9b5b5a9903
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2017-01-29 13:39:40 -0500

    Fixed raml layer

--- layers/raml/packages.el
+++ layers/raml/packages.el
@@ -60,7 +60,7 @@ Each entry is either:
       - A list beginning with the symbol `recipe' is a melpa
         recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
 
-(defun raml/init-yaml-mode ()
+(defun raml/post-init-yaml-mode ()
   (use-package yaml-mode
     :defer t
     :mode ("\\.raml$" . yaml-mode)
